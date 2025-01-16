0334525e4aca90d661218fbace28c682328f6a13
Huy Dang <caohuy@live.com>
2025-01-16 11:36:44 +0700

    Build file

--- build.cmd
+++ build.cmd
@@ -1,2 +1,19 @@
 @echo off
-dotnet publish AchievementsBooster -c "Release" -o "out"
+
+set TARGET_DIR=out
+set ZIP_FILE=AchievementsBooster.zip
+
+if exist "%TARGET_DIR%" (
+  rmdir /s /q "%TARGET_DIR%"
+)
+
+dotnet publish AchievementsBooster -c "Release" -o "%TARGET_DIR%"
+
+copy README.md "%TARGET_DIR%"
+copy NOTICE.md "%TARGET_DIR%"
+
+pushd "%TARGET_DIR%"
+tar -a -c -f "../%ZIP_FILE%" *
+
+popd
+move "%ZIP_FILE%" "%TARGET_DIR%"
